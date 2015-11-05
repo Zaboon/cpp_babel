96f219ec425d24bf387d73f3d8c021739c5716dc
Pinate_n <nicolas.pinatel@epitech.eu>
2015-11-05 12:55:55 +0100

    first commit for contact

---
+++ Babel/Contact.hpp
@@ -0,0 +1,21 @@
+//
+// Created by pinate_n on 11/5/15.
+//
+
+#pragma once
+
+#include							<fstream>
+#include							<iostream>
+#include							<string>
+#include							<vector>
+
+
+class								Contact {
+
+private:
+	std::string						_user;
+	std::vector<std::string>		_list;
+
+public:
+
+};
